3df8c08813af012926ce16ff929c55749d84b7de
TJ <lagrimasyeah@gmail.com>
2025-03-12 15:00:04 -0500

    Update streamlit_app.py
    Misspelling dataframe

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,7 +7,7 @@ import requests
 
 smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
 # st.text(smoothiefroot_response.json())
-sf_df = st.datafram(data=smoothiefroot_response.json(), use_container_width=true)
+sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=true)
 
 
 # Write directly to the app
