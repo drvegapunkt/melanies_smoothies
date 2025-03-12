6b8e57c8346de2a6071d109ec78030e0bad326f4
TJ <lagrimasyeah@gmail.com>
2025-03-12 14:55:37 -0500

    Update streamlit_app.py
    Adding smoothiefroot package

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,6 +3,10 @@ import streamlit as st
 # Not used in Streamlit SniS, only SiS
 # from snowflake.snowpark.context import get_active_session
 from snowflake.snowpark.functions import col
+import requests
+
+smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+st.text(smoothiefroot_response)
 
 helpful_links = [
     "https://docs.streamlit.io",
